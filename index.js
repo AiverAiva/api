784f25bd824bcaa2265dfff0098c027825bb3bce
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2023-03-19 05:38:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { createServer } = require('http');
-
-const profileRouter = require('./api/profile.js');
-
-app.use('/profile', profileRouter);
-app.get('/', (req, res) => {
-    res.send("this is just a router page")
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+// Export the Express API
+module.exports = app;
+
+// const express = require('express');
+// const app = express();
+// const { createServer } = require('http');
+
+// const profileRouter = require('./api/profile.js');
+
+// app.use('/profile', profileRouter);
+// app.get('/', (req, res) => {
+//     res.send("this is just a router page")
+// });
  
-const server = createServer(app);
-module.exports = (req, res) => {
-  server(req, res);
-};
+// const server = createServer(app);
+// module.exports = (req, res) => {
+//   server(req, res);
+// };
