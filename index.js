e9d16f37903df9b2767409e53939f52d85dfe0d7
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2023-03-19 05:23:28 +0800

    q

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use('/profile', profileRouter);
 app.get('/', (req, res) => {
     res.send("this is just a router page")
 });
-
+ 
 const server = createServer(app);
 module.exports = (req, res) => {
   server(req, res);
