e10cec8a6fde01393832031a4358988e9be9deb1
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2023-10-25 23:17:37 +0800

    perf: improve input experience

--- src/pages/api/wynncraft/guildPrefix/[prefix].js
+++ src/pages/api/wynncraft/guildPrefix/[prefix].js
@@ -6,13 +6,22 @@ export default async (req, res) => {
     } = req;
 
     const prefixList = await axios.get('https://raw.githubusercontent.com/AiverAiva/data-crawler/main/data/wynncraft/guildprefix.json')
-    
-    
-    if(prefix in prefixList.data){
+
+    if (prefix in prefixList.data) {
         res.statusCode = 200;
         res.json(prefixList.data[prefix])
-    }else{
-        res.status(400).json({ error: "Guild not found or other issue(check api.weikuwu.me for more infomation)" });
+    } else {
+        const lowercaseObjects = {};
+        for (const key in prefixList.data) {
+            const lowercaseKey = key.toLowerCase();
+            lowercaseObjects[lowercaseKey] = prefixList.data[key];
+        }
+        if (prefix in lowercaseObjects) {
+            res.statusCode = 200;
+            res.json(lowercaseObjects[prefix])
+        }else{
+            res.status(400).json({ error: "Guild not found or other issue(check api.weikuwu.me for more infomation)" });
+        }
     }
-    
-  }
+
+}
