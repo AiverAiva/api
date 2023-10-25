bcdbd49597983566d9abf205b173108b7d2a6a63
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2023-10-25 23:38:46 +0800

    refactor: forgor to update this one :D

--- src/pages/api/wynncraft/guildName/[guild].js
+++ src/pages/api/wynncraft/guildName/[guild].js
@@ -8,14 +8,28 @@ export default async (req, res) => {
     
     const prefixList = await axios.get('https://raw.githubusercontent.com/AiverAiva/data-crawler/main/data/wynncraft/guildprefix.json')
     
-    if(guild in prefixList.data){
-        res.status(200).json(prefixList.data[guild]);
-    }else{
-        const guildData = await axios.get(`https://api.wynncraft.com/public_api.php?action=guildStats&command=${guild}`)
-        if ('error' in guildData.data){
-            res.status(400).json({ error: "Unknown guild name or prefix, this is case sensitive btw(check api.weikuwu.me for more infomation)" });
+
+    if (guild in prefixList.data) {
+        res.statusCode = 200;
+        res.json({guildName: prefixList.data[guild]})
+    } else {
+        const lowercaseObjects = {};
+        for (const key in prefixList.data) {
+            const lowercaseKey = key.toLowerCase();
+            lowercaseObjects[lowercaseKey] = prefixList.data[key];
+        }
+        if (guild in lowercaseObjects) {
+            res.statusCode = 200;
+            res.json({ guildName: lowercaseObjects[guild]})
         }else{
-            res.status(200).json(guild);
+            const guildData = await axios.get(`https://api.wynncraft.com/public_api.php?action=guildStats&command=${guild}`)
+            if ('error' in guildData.data){
+                res.status(400).json({ error: "Unknown guild name or prefix, this is case sensitive btw(check api.weikuwu.me for more infomation)" });
+            }else{
+                res.status(200).json({guildName: prefixList.data[guild]});
+            }
+            res.status(400).json({ error: "Guild not found or other issue(check api.weikuwu.me for more infomation)" });
         }
     }
+
   }
